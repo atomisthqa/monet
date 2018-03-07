e209b096c9b5d7150c7aea4fa3cb12a6b51cc634
Jessica Kerr <jessitron@gmail.com>
2018-03-07 14:52:00 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/money/MonetApplication.java
@@ -0,0 +1,12 @@
+package com.money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonetApplication.class, args);
+	}
+}
